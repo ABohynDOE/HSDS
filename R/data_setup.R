200235a7c5bae22188b27e8b9264689c2f521470
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2024-06-20 10:33:25 +0200

    Add the internal function "data_setup" to setup file for a new data set

---
+++ R/data_setup.R
@@ -0,0 +1,72 @@
+#' Setup for a new data set
+#'
+#' Generate the three files needed for a new data set:
+#' - A file to import the raw data as an R data set
+#' - A file to generate the documentation of the data set
+#' - A file to contain the examples for the data generated
+#' @param data character. Name of the data set for which the files are created. Must be an existing `.dat` file in the `data-raw` folder.
+#' @md
+#' @keywords internal
+data_setup <- function(data) {
+  # ARG CHECK ----
+  if (!is.character(data)) {
+    cli::cli_abort(c("{.var data} must be a character", "x" = "You've supplied a {.cls {class(data)}}"))
+  }
+  if (!fs::file_exists(here::here("data-raw", "data-files", paste0(data, ".dat")))) {
+    cli::cli_abort(
+      c("{.var data} must be the name of an existing data file", "x" = "File {.file {paste0(data, '.dat')}} doesn't exist in {.path data-raw/data-files}")
+    )
+  }
+  # DATA GENERATION ----
+  # Location of the file to convert the raw data
+  data_raw_path <- here::here("data-raw", paste0(data, ".R"))
+  # Generate the empty file
+  fs::file_create(data_raw_path)
+  # Generic code to write to all files
+  text <- c(
+    glue::glue("# {toupper(data)} - data raw"),
+    "library(dplyr)",
+    "",
+    glue::glue("{data} <- readr::read_delim("),
+    glue::glue('  file = here::here("data-raw", "data-files", "{data}.dat"),'),
+    ")",
+    "",
+    glue::glue("usethis::use_data({data}, overwrite = TRUE)")
+  )
+  # Write generic code to empty file
+  writeLines(text, con = data_raw_path)
+  # Info message to user
+  cli::cli_alert_info("Data generation file for '{data}' created at: {.path {data_raw_path}}")
+
+  # DATA DOCUMENTATION ----
+  # Name of the data documentation file
+  data_doc_path <- here::here("R", paste0(data, ".R"))
+  # Generate the empty file
+  fs::file_create(data_doc_path)
+  # Generic code to write to documentation files
+  text <- c(
+    "#' ",
+    "#' ",
+    "#' ",
+    "#' ",
+    "#' @format A data frame with X rows and X columns:",
+    "#' \\describe{",
+    "#'   \\item{}{}",
+    "#' }",
+    "#' ",
+    paste0("#' @example inst/examples/", data, ".R"),
+    "#' ",
+    "#' @source {}",
+    paste0('"', data, '"')
+  )
+  # Write text to raw data file
+  writeLines(text, con = data_doc_path)
+  # Info message to user
+  cli::cli_alert_info("Data documentation file for '{data}' created at: {.path {data_doc_path}}")
+
+  # DATA EXAMPLES ----
+  data_example_path <- here::here("inst", "examples", paste0(data, ".R"))
+  fs::file_create(data_example_path)
+  # Info message to user
+  cli::cli_alert_info("Data example file for '{data}' created at: {.path {data_example_path}}")
+}
